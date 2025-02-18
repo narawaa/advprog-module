1515d5e9bdac673e5402a457fdda505e6578ec5d
narawaa <nashwa.ghania@ui.ac.id>
2025-02-19 00:43:59 +0700

    fix: add comment explaining empty method

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -18,6 +18,7 @@ class ProductRepositoryTest {
 
     @BeforeEach
     void setUp() {
+        // This method is intentionally left empty as no setup is required before each test
     }
 
     @Test
