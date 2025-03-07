409a0694c3e401254341d17974ec93e1762079e5
narawaa <nashwa.ghania@ui.ac.id>
2025-03-07 16:05:21 +0700

    [GREEN] Implement PaymentServiceImpl class and fix bug

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Map;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+    @Autowired
     private PaymentRepository paymentRepository;
 
     @Override
